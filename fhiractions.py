580d5788699a1276570be4b526f57217c8670ee6
Bell Eapen <github_public@gulfdoctor.net>
2020-09-30 13:20:36 -0400

    fhiractions in progress

--- fhiractions.py
+++ fhiractions.py
@@ -1,30 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -45,224 +18,33 @@ from rasa_sdk.forms import FormAction
 # http://hapi.fhir.org/baseR4/Patient?_id=1271537
 # http://hapi.fhir.org/baseR4/Encounter?subject=94ed9c20-7f16-4e27-aa79-66c883fccd19
 
-ENDPOINTS = {
-    "base": "https://data.medicare.gov/resource/{}.json",
-    "xubh-q36u": {
-        "city_query": "?city={}",
-        "zip_code_query": "?zip_code={}",
-        "id_query": "?provider_id={}"
-    },
-    "b27b-2uc7": {
-        "city_query": "?provider_city={}",
-        "zip_code_query": "?provider_zip_code={}",
-        "id_query": "?federal_provider_number={}"
-    },
-    "9wzi-peqs": {
-        "city_query": "?city={}",
-        "zip_code_query": "?zip={}",
-        "id_query": "?provider_number={}"
-    }
-}
 
-FACILITY_TYPES = {
-    "hospital":
-        {
-            "name": "hospital",
-            "resource": "xubh-q36u"
-        },
-    "nursing_home":
-        {
-            "name": "nursing home",
-            "resource": "b27b-2uc7"
-        },
-    "home_health":
-        {
-            "name": "home health agency",
-            "resource": "9wzi-peqs"
-        }
-}
+# Resource (Encounter) - parameter (subject) - qualifier (=) - value (585457)
+# http://hapi.fhir.org/baseR4/Encounter?subject=585457&&&
+ENDPOINT = "http://hapi.fhir.org/baseR4/{}?{}{}{}&{}{}{}&{}{}{}&{}{}{}"
 
-
-def _create_path(base: Text, resource: Text,
-                 query: Text, values: Text) -> Text:
-    """Creates a path to find provider using the endpoints."""
-
-    if isinstance(values, list):
-        return (base + query).format(
-            resource, ', '.join('"{0}"'.format(w) for w in values))
-    else:
-        return (base + query).format(resource, values)
-
-
-def _find_facilities(location: Text, resource: Text) -> List[Dict]:
-    """Returns json of facilities matching the search criteria."""
-
-    if str.isdigit(location):
-        full_path = _create_path(ENDPOINTS["base"], resource,
-                                 ENDPOINTS[resource]["zip_code_query"],
-                                 location)
-    else:
-        full_path = _create_path(ENDPOINTS["base"], resource,
-                                 ENDPOINTS[resource]["city_query"],
-                                 location.upper())
-    #print("Full path:")
-    #print(full_path)
-    results = requests.get(full_path).json()
-    return results
-
-
-def _resolve_name(facility_types, resource) ->Text:
-    for key, value in facility_types.items():
-        if value.get("resource") == resource:
-            return value.get("name")
-    return ""
-
-
-class FindFacilityTypes(Action):
-    """This action class allows to display buttons for each facility type
-    for the user to chose from to fill the facility_type entity slot."""
+class SearchFhirEndpoint(Action):
+    """This action class retrieves a FHIR bundle 
+    according to a search criteria."""
 
     def name(self) -> Text:
         """Unique identifier of the action"""
 
-        return "find_facility_types"
-
-    def run(self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List:
-
-        buttons = []
-        for t in FACILITY_TYPES:
-            facility_type = FACILITY_TYPES[t]
-            payload = "/inform{\"facility_type\": \"" + facility_type.get(
-                "resource") + "\"}"
-
-            buttons.append(
-                {"title": "{}".format(facility_type.get("name").title()),
-                 "payload": payload})
-
-        # TODO: update rasa core version for configurable `button_type`
-        dispatcher.utter_button_template("utter_greet", buttons, tracker)
-        return []
-
-
-class FindHealthCareAddress(Action):
-    """This action class retrieves the address of the user's
-    healthcare facility choice to display it to the user."""
-
-    def name(self) -> Text:
-        """Unique identifier of the action"""
-
-        return "find_healthcare_address"
+        return "search_fhir_endpoint"
 
     def run(self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict]:
 
-        facility_type = tracker.get_slot("facility_type")
-        healthcare_id = tracker.get_slot("facility_id")
-        full_path = _create_path(ENDPOINTS["base"], facility_type,
-                                 ENDPOINTS[facility_type]["id_query"],
-                                 healthcare_id)
+        fhir_resource = tracker.get_slot("fhir_resource")
+        search_param = tracker.get_slot("search_param")
+        search_qualifier = tracker.get_slot("search_qualifier")
+        search_value = tracker.get_slot("search_value")
+        full_path = ENDPOINT.format(fhir_resource, search_param, search_qualifier, search_value)
         results = requests.get(full_path).json()
-        if results:
-            selected = results[0]
-            if facility_type == FACILITY_TYPES["hospital"]["resource"]:
-                address = "{}, {}, {} {}".format(selected["address"].title(),
-                                                 selected["city"].title(),
-                                                 selected["state"].upper(),
-                                                 selected["zip_code"].title())
-            elif facility_type == FACILITY_TYPES["nursing_home"]["resource"]:
-                address = "{}, {}, {} {}".format(selected["provider_address"].title(),
-                                                 selected["provider_city"].title(),
-                                                 selected["provider_state"].upper(),
-                                                 selected["provider_zip_code"].title())
-            else:
-                address = "{}, {}, {} {}".format(selected["address"].title(),
-                                                 selected["city"].title(),
-                                                 selected["state"].upper(),
-                                                 selected["zip"].title())
-
-            return [SlotSet("facility_address", address)]
-        else:
-            print("No address found. Most likely this action was executed "
-                  "before the user choose a healthcare facility from the "
-                  "provided list. "
-                  "If this is a common problem in your dialogue flow,"
-                  "using a form instead for this action might be appropriate.")
-
-            return [SlotSet("facility_address", "not found")]
+        return results
+    
 
 
-class FacilityForm(FormAction):
-    """Custom form action to fill all slots required to find specific type
-    of healthcare facilities in a certain city or zip code."""
 
-    def name(self) -> Text:
-        """Unique identifier of the form"""
-
-        return "facility_form"
-
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-
-        return ["facility_type", "location"]
-
-    def slot_mappings(self) -> Dict[Text, Any]:
-        return {"facility_type": self.from_entity(entity="facility_type",
-                                                  intent=["inform",
-                                                          "search_provider"]),
-                "location": self.from_entity(entity="location",
-                                             intent=["inform",
-                                                     "search_provider"])}
-
-    def submit(self,
-               dispatcher: CollectingDispatcher,
-               tracker: Tracker,
-               domain: Dict[Text, Any]
-               ) -> List[Dict]:
-        """Once required slots are filled, print buttons for found facilities"""
-
-        location = tracker.get_slot('location')
-        facility_type = tracker.get_slot('facility_type')
-
-        results = _find_facilities(location, facility_type)
-        button_name = _resolve_name(FACILITY_TYPES, facility_type)
-        if len(results) == 0:
-            dispatcher.utter_message(
-                "Sorry, we could not find a {} in {}.".format(button_name,
-                                                              location.title()))
-            return []
-
-        buttons = []
-        # limit number of results to 3 for clear presentation purposes
-        for r in results[:3]:
-            if facility_type == FACILITY_TYPES["hospital"]["resource"]:
-                facility_id = r.get("provider_id")
-                name = r["hospital_name"]
-            elif facility_type == FACILITY_TYPES["nursing_home"]["resource"]:
-                facility_id = r["federal_provider_number"]
-                name = r["provider_name"]
-            else:
-                facility_id = r["provider_number"]
-                name = r["provider_name"]
-
-            payload = "/inform{\"facility_id\":\"" + facility_id + "\"}"
-            buttons.append(
-                {"title": "{}".format(name.title()), "payload": payload})
-
-        if len(buttons) == 1:
-            message = "Here is a {} near you:".format(button_name)
-        else:
-            if button_name == "home health agency":
-                button_name = "home health agencie"
-            message = "Here are {} {}s near you:".format(len(buttons),
-                                                         button_name)
-
-        # TODO: update rasa core version for configurable `button_type`
-        dispatcher.utter_button_message(message, buttons)
-
-        return []
